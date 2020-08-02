721249bed08b59415fd34e0891c2846e622adbf5
IbrahemAtef <bulbul.20001@gmail.com>
2020-08-02 15:52:31 +0300

    db v1.1

--- db/index.js
+++ db/index.js
@@ -5,12 +5,14 @@ const {
   GraphQLList,
   GraphQLSchema,
   GraphQLNonNull,
+  GraphQLBoolean,
 } = require('graphql');
+const knex = require('./database');
 
 const UserType = new GraphQLObjectType({
-  name: 'User',
+  name: 'user',
   fields: () => ({
-    id: { type: new GraphQLNonNull(GraphQLString) },
+    id: { type: new GraphQLNonNull(GraphQLInt), unique: true },
     username: { type: new GraphQLNonNull(GraphQLString) },
     email: { type: new GraphQLNonNull(GraphQLString) },
     password: { type: new GraphQLNonNull(GraphQLString) },
@@ -22,7 +24,51 @@ const UserType = new GraphQLObjectType({
   }),
 });
 
-// id: { type: new GraphQLNonNull(GraphQLString), unique: true },
+const CartType = new GraphQLObjectType({
+  name: 'cart',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLInt), unique: true },
+    userID: { type: new GraphQLNonNull(GraphQLInt) },
+    productID: { type: new GraphQLNonNull(GraphQLInt) },
+    sold: { type: new GraphQLNonNull(GraphQLBoolean) },
+  }),
+});
+
+const ProductType = new GraphQLObjectType({
+  name: 'product',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLInt), unique: true },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    category: { type: new GraphQLNonNull(GraphQLString) },
+    price: { type: new GraphQLNonNull(GraphQLInt) },
+    userID: { type: new GraphQLNonNull(GraphQLInt) },
+    rating: { type: new GraphQLNonNull(GraphQLInt) },
+    quantity: { type: new GraphQLNonNull(GraphQLInt) },
+  }),
+});
+
+const CommentType = new GraphQLObjectType({
+  name: 'comment',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLInt), unique: true },
+    userID: { type: new GraphQLNonNull(GraphQLInt) },
+    postID: { type: new GraphQLNonNull(GraphQLInt) },
+    text: { type: new GraphQLNonNull(GraphQLString) },
+    date: { type: new GraphQLNonNull(GraphQLString) },
+  }),
+});
+
+const PostType = new GraphQLObjectType({
+  name: 'post',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLInt), unique: true },
+    userID: { type: new GraphQLNonNull(GraphQLInt) },
+    likes: { type: new GraphQLNonNull(GraphQLInt) },
+    date: { type: new GraphQLNonNull(GraphQLString) },
+    text: { type: new GraphQLNonNull(GraphQLString) },
+    image: { type: new GraphQLNonNull(GraphQLString) },
+  }),
+});
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -30,13 +76,54 @@ const RootQuery = new GraphQLObjectType({
     user: {
       type: UserType,
       args: {
-        id: { type: new GraphQLNonNull(GraphQLString) },
-        resolve(root, args) {},
+        id: { type: new GraphQLNonNull(GraphQLInt) },
+      },
+      async resolve(root, args) {
+        return await knex('user').select().where({ id: args.id });
       },
     },
     users: {
       type: new GraphQLList(UserType),
-      resolve(root, args) {},
+      async resolve(root, args) {
+        return await knex('user').select();
+      },
+    },
+    cart: {
+      type: CartType,
+      args: {
+        // what we need as an arguments
+        userID: { type: new GraphQLNonNull(GraphQLInt) },
+      },
+      async resolve(root, args) {
+        return await knex('cart').select().where({ userID: args.userID });
+      },
+    },
+    product: {
+      type: ProductType,
+      args: {
+        userID: { type: new GraphQLNonNull(GraphQLInt) },
+      },
+      async resolve(root, args) {
+        return await knex('product').select().where({ userID: args.userID });
+      },
+    },
+    comment: {
+      type: CommentType,
+      args: {
+        postID: { type: new GraphQLNonNull(GraphQLInt) },
+      },
+      async resolve(root, args) {
+        return await knex('comment').select().where({ postID: args.postID });
+      },
+    },
+    post: {
+      type: PostType,
+      args: {
+        userID: { type: new GraphQLNonNull(GraphQLInt) },
+      },
+      async resolve(root, args) {
+        return await knex('post').select().where({ userID: args.userID });
+      },
     },
   },
 });
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const expressGraphQL = require('express-graphql');
+const expressGraphQL = require('express-graphql').graphqlHTTP;
 const schema = require('../db/index');
 const app = express();
 
